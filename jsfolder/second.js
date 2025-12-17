1ebf4619bdc1107699e4095d8053ed5446aad6d3
Shivani Pathak <kshivanipathak2126@gmail.com>
2025-12-17 15:09:10 +0000

    save'em

---
+++ jsfolder/second.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+// alert("hello") // we are using nodejs not browser 
+// search alert mdn in google for documentation of alert!!!
+// two most important documentations to refer for js are tc39(ECMA stds) and mdn
+console.log("Shivani Pathak")
+
+console.log(3+3)
+let name="Shivani"
+let age=19
+let userLogin=true
+// Primitive datatypes
+// number 2 power 53
+// bigInt if any num is larger
+//string "" ''
+//boolean true false
+//null standlone value we can assign null as value , null is of type object
+// undefined ,variable unassigned 
+//symbols to find the uniqueness of components
+
+//object
+console.log(typeof null) // type=> object
+console.log(typeof undefined) // type=> undefined
